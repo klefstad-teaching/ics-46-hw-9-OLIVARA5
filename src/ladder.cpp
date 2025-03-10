b11a6f3e3c39140396a84aa93d4643d65fcbfac1
OLIVARA5 <OLIVARA5@uci.edu>
2025-03-10 03:10:46 +0000

    Implemented generate_word_ladder using pseudocode provided in read up.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -14,10 +14,63 @@ void error(string word1, string word2, string msg){
     cout << msg << word1 << " and " << word2 << endl;
 }
 
-vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
-    
+bool is_adjacent(const string& word1, const string& word2){
+    int adjacent = 0;
+    if(word1.length() != word2.length()){
+        return false;
+    }
+    for(int i = 0; i < word1; ++i){
+        if(word1[i] != word2[i]){
+            adjacent++;
+        }
+        if(adjacent > 1){
+            return false;
+        }
+    }
+    return adjacent == 1;
 }
 
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+    set<string> visted;
+    visted.insert(begin_word);
+    while(!ladder_queue.empty()){
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder_queue.back();
+        for(const auto&word: word_list){
+            if is_adjacent(last_word, word){
+                if(visited.find(word) == visited.end()){
+                    visited.insert(word);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if(word == end_word){
+                        return new_ladder;
+                    }
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    return {};
+}
+
+
+
+void load_words(set<string> &word_list, const string& file_name) {
+    ifstream in(file_name); 
+    if (!in) {
+        throw runtime_error("Can't open input file");
+    }
+
+    string word;
+    while (file >> word) { 
+        word_list.insert(word); 
+    }
+
+    in.close();
+}
 
 void print_word_ladder(const vector<string>& ladder) {
     for (const string& word : ladder) {
