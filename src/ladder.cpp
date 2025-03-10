60150cf042f9ee8bdb15297fdf4c52e1a10e9684
OLIVARA5 <OLIVARA5@uci.edu>
2025-03-10 05:23:45 +0000

    Fix is_adjacent.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -64,7 +64,7 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         ladder_queue.pop();
         string last_word = ladder.back();
         for(const auto&word: word_list){
-            if is_adjacent(last_word, word){
+            if (is_adjacent(last_word, word)){
                 if(visited.find(word) == visited.end()){
                     visited.insert(word);
                     vector<string> new_ladder = ladder;
